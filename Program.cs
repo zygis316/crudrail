94c5173329068443e5d23aedb481242fdbb6e804
zygis316 <zygis316@gmail.com>
2025-10-23 14:10:48 +0300

    fix postgresql conn parsing for railway

--- Program.cs
+++ Program.cs
@@ -9,16 +9,36 @@ builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("BookContext")
                        ?? Environment.GetEnvironmentVariable("DATABASE_URL");
 
+//if (connectionString != null && connectionString.Contains("postgres"))
+//{
+//    builder.Services.AddDbContext<BookContext>(options =>
+//        options.UseNpgsql(connectionString));
+//}
+//else
+//{
+//    builder.Services.AddDbContext<BookContext>(options =>
+//        options.UseSqlServer(connectionString ?? ""));
+//}
+
+// Updated code to handle Heroku-style DATABASE_URL for PostgreSQL
 if (connectionString != null && connectionString.Contains("postgres"))
 {
+    // Convert DATABASE_URL to a Npgsql-compatible connection string
+    var databaseUri = new Uri(connectionString);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    var pgConn = $"Host={databaseUri.Host};Port={databaseUri.Port};Database={databaseUri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+
     builder.Services.AddDbContext<BookContext>(options =>
-        options.UseNpgsql(connectionString));
+        options.UseNpgsql(pgConn));
 }
 else
 {
     builder.Services.AddDbContext<BookContext>(options =>
         options.UseSqlServer(connectionString ?? ""));
 }
+
+
 
 var app = builder.Build();
 
